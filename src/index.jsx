30d76c5d0779e9d59d10a43bf5618a0ec4e2246a
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-04-27 13:59:53 -0300

    :recycle: refactor: Mudando de BrowserRouter para HashRouter, por causa do GitHub Pages.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/index.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import GithubAPIDataProvider from "./context/GithubAPIDataProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <GithubAPIDataProvider>
       <App />
     </GithubAPIDataProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
